d178b0815eac97dfff5772de1fec7a7bfedcf858
Janilto Oliveira <janiltojack@yahoo.com.br>
2022-10-21 00:04:02 -0300

    add Warning Error

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 
 
 
@@ -11,7 +16,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatCardModule,
     MatTableModule,
     MatToolbarModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule
   ],
 
 
